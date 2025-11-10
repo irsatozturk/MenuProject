9baa2bed4daeb3c34d6f09b953607cc16f73ec6b
Irsat Ozturk <irsatozturk@gmail.com>
2025-11-11 02:14:03 +0300

    faz 2 ilk aşama bitti - servis katmanı ve api endpoint

---
+++ MenuProject.API/Controllers/MenuController.cs
@@ -0,0 +1,30 @@
+﻿using MenuProject.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MenuProject.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MenuController : ControllerBase
+    {
+        private readonly IMenuService _menuService;
+
+        public MenuController(IMenuService menuService)
+        {
+            _menuService = menuService;
+        }
+
+        [HttpGet("Default")]
+        public async Task<IActionResult> GetDefaultMenu(string language = "tr")
+        {
+            var menu = await _menuService.GetDefaultMenuAsync(language);
+
+            if (menu == null)
+            {
+                return NotFound("Varsayılan menü bulunamadı.");
+            }
+
+            return Ok(menu);
+        }
+    }
+}
--- MenuProject.API/Program.cs
+++ MenuProject.API/Program.cs
@@ -1,5 +1,6 @@
 using MenuProject.API.Data;
 using Microsoft.EntityFrameworkCore;
+using MenuProject.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,15 +8,23 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
+builder.Services.AddScoped<IMenuService, MenuService>();
+
 builder.Services.AddControllers();
 
-builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();  //builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+//if (app.Environment.IsDevelopment())
+//{
+//    app.MapOpenApi();
+//}
+
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(); // Bu, /swagger/index.html adresini oluþturur
 }
 
 app.UseHttpsRedirection();
